179d783872fe189348629df4b1beb6e96eaa9fcb
AlvesMeloMatheus <teteu.melo4@.gmail.com>
2023-04-11 11:27:12 -0300

    pi-food -> me hizo aprender mucho, pero, aun me falta

--- api/src/models/Recipe.js
+++ api/src/models/Recipe.js
@@ -1,12 +1,37 @@
 const { DataTypes } = require('sequelize');
+
+const {Diets} = require('./Diets');
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('recipe', {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false,
+    },
     name: {
       type: DataTypes.STRING,
       allowNull: false,
     },
+    image: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    summary: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    health_score: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    instructions: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
   });
 };
+
--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,30 @@
 import './App.css';
+//--------- ^^ css ^^ ----------
+
+// ------------ components -----------
+import Landing from './components/Landing/Landing';
+import Home from './components/Home/Home';
+import NotFound from './components/NotFound/NotFound';
+// -----------------------------------
+import { Routes, Route, useLocation } from 'react-router-dom';
 
 function App() {
+
+  const location = useLocation();
+  console.log(location.pathname);
+
+
+
   return (
     <div className="App">
-      <h1>Henry Food</h1>
+
+
+      <Routes>
+        <Route exact path="/" element={<Landing/>} />
+        <Route exact path="/home" element={<Home/>} />
+        <Route exact path="*" element={<NotFound/>} />
+      </Routes>
+
     </div>
   );
 }
